672710549a004bd4211cab25ed6004e3e9f2e2a9
Stephen Gruzin <stephengrzn@gmail.com>
2024-05-06 14:24:44 -0700

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,38 @@
+<main>
+    Currently under construction
+    <a href="https://g.co/kgs/z7nyXas" target="_blank">Check Out My Google Profile</a>
+</main>
+
+<style>
+    :global(body) {
+        width: 100vw;
+        height: 100vh;
+        height: 100dvh;
+        background-image: url(/excavator.png);
+        background-size: cover;
+        background-position: 50% 50%;
+    }
+
+    main {
+        width: 100%;
+        height: 100%;
+        background-color: #00000077;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        font-family: "Open Sans", sans-serif;
+        color: #ffffff;
+        font-size: clamp(1.5rem, 2.1271rem + -2.8667vw, 0.15625rem);
+        flex-direction: column;
+        gap: 20px;
+    }
+
+    a {
+        background-color: #ff7b00;
+        font-size: 0.9rem;
+        padding: 15px;
+        text-decoration: none;
+        color: #ffffff;
+        border-radius: 100vw;
+    }
+</style>
